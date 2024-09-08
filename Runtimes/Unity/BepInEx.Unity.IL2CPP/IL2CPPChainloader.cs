ec06ac14a81c8572428a1273710aa363e74995a3
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-09-08 15:45:19 +0200

    Automatically load all interop assemblies before loading plugins (#777)
    Assemblies are often loaded on the il2cpp side but not on the mono side, causing confusion when using reflection.
    This will load all interop assemblies right before plugins are loaded, which should mostly avoid these issues at a low time cost of around 40ms.
    
    (cherry picked from commit cbd70eb2f085090751af32626f6c0f12c06efa4f)

--- Runtimes/Unity/BepInEx.Unity.IL2CPP/IL2CPPChainloader.cs
+++ Runtimes/Unity/BepInEx.Unity.IL2CPP/IL2CPPChainloader.cs
@@ -103,6 +103,8 @@ public class IL2CPPChainloader : BaseChainloader<BasePlugin>
 
                 unhook = true;
 
+                Il2CppInteropManager.PreloadInteropAssemblies();
+
                 Instance.Execute();
             }
             catch (Exception ex)
