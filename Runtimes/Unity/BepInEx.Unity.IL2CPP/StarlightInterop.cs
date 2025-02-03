46919a522319523507300efeefef473eb2cb3c66
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-02-03 11:48:38 -0500

    change name

--- Runtimes/Unity/BepInEx.Unity.IL2CPP/StarlightInterop.cs
+++ Runtimes/Unity/BepInEx.Unity.IL2CPP/StarlightInterop.cs
@@ -7,19 +7,19 @@ namespace BepInEx.Unity.IL2CPP;
 
 public static unsafe partial class StarlightInterop
 {
-    [LibraryImport("brutha", EntryPoint = "write_log", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("starlight", EntryPoint = "write_log", StringMarshalling = StringMarshalling.Utf8)]
     public static unsafe partial void write_log([MarshalAs(UnmanagedType.LPStr)] string message);
 
-    [LibraryImport("brutha", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("starlight", StringMarshalling = StringMarshalling.Utf8)]
     public static unsafe partial void flush_log();
 
-    [LibraryImport("brutha")]
+    [LibraryImport("starlight")]
     public static unsafe partial IntPtr hook(IntPtr target, IntPtr detour);
 
-    [LibraryImport("brutha")]
+    [LibraryImport("starlight")]
     public static unsafe partial void unhook(IntPtr target);
 
-    [LibraryImport("brutha")]
+    [LibraryImport("starlight")]
     public static unsafe partial void thread_suspend_reload();
     
     public class InteropWriter : TextWriter
