da5f033f53d7058d6acf396a487b7ae63a9dbbcd
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-05-05 18:54:33 -0400

    code improvement

--- Runtimes/Unity/BepInEx.Unity.IL2CPP/StarlightInterop.cs
+++ Runtimes/Unity/BepInEx.Unity.IL2CPP/StarlightInterop.cs
@@ -7,19 +7,21 @@ namespace BepInEx.Unity.IL2CPP;
 
 public static unsafe partial class StarlightInterop
 {
-    [LibraryImport("starlight", EntryPoint = "write_log", StringMarshalling = StringMarshalling.Utf8)]
+    private const string LIBRARY_NAME = "starlight";
+
+    [LibraryImport(LIBRARY_NAME, EntryPoint = "write_log", StringMarshalling = StringMarshalling.Utf8)]
     public static unsafe partial void write_log([MarshalAs(UnmanagedType.LPStr)] string message);
 
-    [LibraryImport("starlight", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(LIBRARY_NAME, StringMarshalling = StringMarshalling.Utf8)]
     public static unsafe partial void flush_log();
 
-    [LibraryImport("starlight")]
+    [LibraryImport(LIBRARY_NAME)]
     public static unsafe partial IntPtr hook(IntPtr target, IntPtr detour);
 
-    [LibraryImport("starlight")]
+    [LibraryImport(LIBRARY_NAME)]
     public static unsafe partial void unhook(IntPtr target);
 
-    [LibraryImport("starlight")]
+    [LibraryImport(LIBRARY_NAME)]
     public static unsafe partial void thread_suspend_reload();
     
     public class InteropWriter : TextWriter
