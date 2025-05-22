178b2ad33b100ffc6056cd2f4680358c3d0055d3
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-05-22 14:42:18 -0400

    update InteropWriter

--- Runtimes/Unity/BepInEx.Unity.IL2CPP/StarlightInterop.cs
+++ Runtimes/Unity/BepInEx.Unity.IL2CPP/StarlightInterop.cs
@@ -5,15 +5,12 @@ using System.Text;
 
 namespace BepInEx.Unity.IL2CPP;
 
-public static unsafe partial class StarlightInterop
+internal static partial class StarlightInterop
 {
     private const string LIBRARY_NAME = "starlight";
 
     [LibraryImport(LIBRARY_NAME, EntryPoint = "write_log", StringMarshalling = StringMarshalling.Utf8)]
     public static unsafe partial void write_log([MarshalAs(UnmanagedType.LPStr)] string message);
-
-    [LibraryImport(LIBRARY_NAME, StringMarshalling = StringMarshalling.Utf8)]
-    public static unsafe partial void flush_log();
 
     [LibraryImport(LIBRARY_NAME)]
     public static unsafe partial IntPtr hook(IntPtr target, IntPtr detour);
@@ -26,16 +23,33 @@ public static unsafe partial class StarlightInterop
     
     public class InteropWriter : TextWriter
     {
+        public override Encoding Encoding => Encoding.UTF8; 
+
+        private readonly StringBuilder buffer = new(1024);
+
         public override void WriteLine(string value)
         {
-            write_log(value + Environment.NewLine);
+            buffer.AppendLine(value);
+            FlushBuffer();
         }
 
         public override void Write(char value)
         {
-            write_log(value.ToString());
+            buffer.Append(value);
+
+            if (value == '\n')
+            {
+                FlushBuffer();
+            }
         }
 
-        public override Encoding Encoding => Encoding.Unicode;
+        public void FlushBuffer()
+        {
+            if (buffer.Length == 0)
+                return;
+
+            write_log(buffer.ToString());
+            buffer.Clear();
+        }
     }
 }
