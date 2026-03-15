44ba08fa3b20466d723e3db97e146498c43e0acc
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2026-03-15 19:48:20 -0300

    Update nuget packages and change native detour parameters

--- Runtimes/Unity/BepInEx.Unity.IL2CPP/Hook/NativeDetourProvider.cs
+++ Runtimes/Unity/BepInEx.Unity.IL2CPP/Hook/NativeDetourProvider.cs
@@ -5,12 +5,6 @@ namespace BepInEx.Unity.IL2CPP.Hook;
 
 public class NativeDetourProvider : IDetourProvider
 {
-    public IDetour Create<TDelegate>(IntPtr original, TDelegate target) where TDelegate : Delegate
-    {
-        var detour = new NativeDetour(original, target);
-        return new CacheDetourWrapper(detour, target);
-    }
-
     public IDetour Create<TDelegate>(IntPtr original, TDelegate target, bool unityFunction) where TDelegate : Delegate
     {
         var detour = new NativeDetour(original, target, unityFunction);
